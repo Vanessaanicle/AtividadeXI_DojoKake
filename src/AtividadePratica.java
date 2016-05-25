8fd3328884d6fc0c778095d4326d8d2faf8d10fc
alexfrs199 <alex_freitas_199@live.com>
2016-05-25 12:02:09 -0300

    Update AtividadePratica.java

--- src/AtividadePratica.java
+++ src/AtividadePratica.java
@@ -24,6 +24,52 @@ public class AtividadePratica {
 		}
 		
 		
+		
+		
+		
+		
+//q4
+		public static String verificarAluno(Aluno alunos[], Professor professores[]){
+		boolean alunoprofessor = false;
+		//int cont = 0;
+		int professor1 = 0, professor2 = 0, professor3 = 0, professor4 = 0, professor5 = 0;
+
+
+		for (int i = 0; i < professores.length ; i++) {
+
+			for (int j = 0; j < professores[i].disciplina.length; j++) {
+				int cont = 0;
+				while( cont < 30 ){
+					if(alunos[cont].disciplina == professores[i].disciplina ){
+						switch (i) {
+						case 0:
+							professor1++;
+							break;
+						case 1:
+							professor2++;
+							break;
+						case 2:
+							professor3++;
+							break;
+						case 3:
+							professor4++;
+							break;
+						case 4:
+							professor5++;
+							break;
+
+						}
+					}
+				}
+			}
+		}
+		
+		return "O professor 1 possui " +professor1+ " alunos." + "\n" + "O professor 2 possui " + professor2+ " alunos." + "\n" + "O professor 3 possui "
+		+ professor3+ " alunos." + "\n" + "O professor 4 possui " + professor4+ " alunos." + "\n" + "O professor 5 possui " + professor5+ " alunos." + "\n";
+	}
+		
+		
+		
 		//Q6
 	public static void ordenacao(double[] notas){
 			
